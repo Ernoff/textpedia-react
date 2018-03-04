825192b7bcefa34e6a3cdd0826378075f9add42f
nanichang <nanipaul68@gmail.com>
2018-03-04 13:09:23 +0100

    asset file error resolved

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 // import 'bootstrap/dist/css/bootstrap.css';
-import 'react-flags-select/css/react-flags-select.css';
+// import 'react-flags-select/css/react-flags-select.css';
 import App from './Components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
