2b11a01b79cac6088924f7ebf5216975258a99e6
nanichang <nanipaul68@gmail.com>
2018-03-07 15:09:16 +0100

    navbar changed

--- src/Components/HowItWork.js
+++ src/Components/HowItWork.js
@@ -5,29 +5,23 @@ import React, { Component } from 'react';
 class Form extends Component {
     render() {
         return (
-
-            <div style={{ overflowY: 'scroll', height: 400 }}>
-                <h5>Using Textpedia is as simple as 1 - 2 - 3!</h5>
+            <div className="scrollbar" id="tp-scroll">
+                <div className="force-overflow">
+                <h3>Using Textpedia is as simple as 1 - 2 - 3!</h3>
                 <p>Signup using your phone number and email address, a token will be emailed to
-                                    you. You'll use this token in the next step to confirm your identity.</p>
+                    you. You'll use this token in the next step to confirm your identity.</p>
                 <p>Confirm your identity by entering the token at the "confirmation" page, upon
-                                    successful completion of the signup process, a welcome email will be sent to
-                                    you with the endpoint phone number you can send your keywords to. </p>
+                    successful completion of the signup process, a welcome email will be sent to
+                    you with the endpoint phone number you can send your keywords to. </p>
                 
                 <p>Whenever you need to conduct preliminary research on any keywords, simply SMS
-                                    that keyword(s) to the endpoint phone number. When the report has been generated,
-                                    it will be sent to your email and an SMS notification will be delivered to you.</p>
-                <h5>Example usage</h5>
-                <p>SMS "Abraham Lincoln" to the endpoint phone number. Note that this is a
-                                    single keyword consisting of two words. Multiple keywords are comma separated
-                                    as shown in the next example.</p>
+                    that keyword(s) to the endpoint phone number. When the report has been generated,
+                    it will be sent to your email and an SMS notification will be delivered to you.</p>
                 
-                <p>SMS "Abraham Lincoln", "Albert Einstein", "Milky Way" to the endpoint phone number.
-                                    Note that the search is for three keywords, the report will consist of three sections,
-                                    one per keyword.</p>
                 <p>Finally, ensure that the keywords are spelled correctly, while Tetxpedia queries are
-                                case insensitive, at this time spell checking is not available. Enjoy the Textpedia service.</p>
+                    case insensitive, at this time spell checking is not available. Enjoy the Textpedia service.</p>
                 <p></p>
+                </div>
             </div>
         );
     }
--- src/Components/layout/Header.js
+++ src/Components/layout/Header.js
@@ -3,7 +3,7 @@ import Form from './Form';
 import Home from '../Home';
 import HowItWork from '../HowItWork';
 import { Link } from "react-router-dom";
-import * as img from "../../assets/img";
+
 import { Switch, Route } from 'react-router-dom';
 import Confirmation from '../Confirmation';
 import Success from '../Success';
@@ -13,7 +13,6 @@ class Header extends Component {
     return (
      
        <header className='uk-grid' data-uk-grid>
-            {/* <a className="sign uk-position-top-right" id="signupbtn" href="">Sign Up</a> */}
             <nav className="sign uk-navbar-container uk-position-top-right" data-uk-navbar>
                 <div className="uk-navbar-right">
                     <ul className="uk-navbar-nav" style={{ color: '#ffffff' }}>
@@ -24,13 +23,13 @@ class Header extends Component {
                 </div>
             </nav>    
             
-        <div className="side uk-width-1-2@m" data-uk-grid>
+        <div className="side uk-width-2-5@m" data-uk-grid>
             <h1 className="uk-text-bold">TEXTPEDIA</h1>
             <p>a <span>TEXT</span>  WIKI<span>PEDIA</span></p>
             <h2>KNOWLEDGE AT YOUR FINGERTIPS</h2>     
         </div>
-        <div className="uk-width-1-2@m" >
-                <div className="form uk-card uk-card-body uk-width-2-3@m uk-overflow-hidden uk-align-center">
+        <div className="uk-width-3-5@m" >
+                <div className="form uk-card uk-card-body uk-width-4-5@m uk-overflow-hidden uk-align-center">
                     <Switch>
                         <Route exact path="/" component={Home} />
                         <Route exact path="/sign-up" component={Form} />
@@ -40,8 +39,7 @@ class Header extends Component {
                     </Switch>
                 </div>
             </div>
-    	</header>   
-      
+    	</header>      
     );
   }
 }
